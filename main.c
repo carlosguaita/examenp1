3cd168f00830a9f41273591d4b31223bc73c5f49
carlosguaita <120821872+carlosguaita@users.noreply.github.com>
2023-05-11 17:00:56 -0500

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main (int argc, char *argv[]) {
     float bx = obtenerBaricentroX(x1,x2,x3);
     float by = obtenerBaricentroX(y1,y2,y3);
 
-    calcularArea(radio);
+    calcularArea(x1,y1,x2,y2,x3,y3,bx,by);
 
     return 0;
 }
